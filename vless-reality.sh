6330ee812cbfbe675b1a309669ec677617f06f91
jyucoeng <hsiangyu.lu@gmail.com>
2025-12-24 14:17:13 +0800

    Update vless-reality.sh

--- vless-reality.sh
+++ vless-reality.sh
@@ -25,7 +25,7 @@ export LANG=en_US.UTF-8
 # ======================================================================
 
 AUTHOR="littleDoraemon"
-VERSION="v2.3.7"
+VERSION="v2.3.8"
 SINGBOX_VERSION="1.12.13"
 
 SERVICE_NAME="sing-box-vless-reality"
