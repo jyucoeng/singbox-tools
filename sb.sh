45268e3f6130827ec2a2b6858da3cdc3095cb7aa
jyucoeng <hsiangyu.lu@gmail.com>
2026-01-25 11:32:13 +0800

    1
    1

--- sb.sh
+++ sb.sh
@@ -286,7 +286,7 @@ create_bashrc_if_missing() {
 create_bashrc_if_missing
 
 # ================== 系统bashrc函数 ==================
-VERSION="1.0.2(2026-01-25)"
+VERSION="1.0.3(2026-01-25)"
 AUTHOR="littleDoraemon"
 
 
