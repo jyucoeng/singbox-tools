a2af1fffeb6ff6ff9b5322997942b238ed60cd62
jyucoeng <hsiangyu.lu@gmail.com>
2026-08-06 17:05:52 +0800

    1
    1

--- sb00.sh
+++ sb00.sh
@@ -22,7 +22,7 @@ SINGBOX_FOLDER_PATH="/root/$SB_FOLDER"
 OLD_SINGBOX_FOLDER="/root/agsb" # 旧路径，用于兼容和清理
 # ================== 文件夹路径配置 结束 ==================
 
-VERSION="1.6.56(2026-08-06)"
+VERSION="1.6.57(2026-08-06)"
 AUTHOR="littleDoraemon"
 
 # Environment variables for controlling CDN host and SNI values
